8d70bdf1e28aafdc45d87d8eaab5d7626ad4c8dd
Krzysztof Błażejewski <reexeeer@gmail.com>
2024-10-12 21:17:16 +0200

    Work page in progres

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,9 +1,43 @@
 import React from 'react';
+import WorkItem from './WorkItem';
+
+const data = [
+    {
+        year: 2020,
+        title: 'Content Creator',
+        duration: '3 Years',
+        details: 'Lorep ipsum, dolor sit amet consectetur adipisicing elit.'
+    },
+    {
+        year: 2020,
+        title: 'Content Creator',
+        duration: '3 Years',
+        details: 'Lorep ipsum, dolor sit amet consectetur adipisicing elit.'
+    },
+    {
+        year: 2020,
+        title: 'Content Creator',
+        duration: '3 Years',
+        details: 'Lorep ipsum, dolor sit amet consectetur adipisicing elit.'
+    },
+    {
+        year: 2020,
+        title: 'Content Creator',
+        duration: '3 Years',
+        details: 'Lorep ipsum, dolor sit amet consectetur adipisicing elit.'
+    },
+]
 
 const Work = () => {
     return (
-        <div>
-            Work
+        <div id='work' className='max-w-[1040px] m-auto md:pl-20 p4 py-16'>
+            <h1 className='text-4xl font-bold text-center text-[#001b5e]'>Work</h1>
+            {
+                data.map((item, index) => (
+                    <WorkItem key={index} year={item.year} title={item.title} duration={item.duration}
+                              details={item.details}/>
+                ))
+            }
         </div>
     );
 };
